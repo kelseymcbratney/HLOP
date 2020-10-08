75a94a7f0e8e120e8d37d82f64cbee4bec24fc6c
kelseymcbratney <kelseymcbratney@gmail.com>
2020-10-08 01:13:18 -0800

    finished

--- hlop.py
+++ hlop.py
@@ -1,4 +1,6 @@
 from board import Board
+from copy import deepcopy
+from solver import Solver
 
 print("How many rows?")
 rows = int(input())
@@ -6,8 +8,38 @@ print("How many cols?")
 cols = int(input())
 print("How many houses?")
 houses = int(input())
-print("Hillclimb or Simulated Annealing? (1, or 2)")
-algo = input()
+print("Hillclimb or Simulated Annealing? (1, or 2) or Press 3 to run them all four times")
+algo = int(input())
 
-b = Board(rows,cols,houses)
-b.launch()
+
+if algo == 1 or algo == 2:
+    b = Board(rows,cols,houses)
+    print("ORIGINAL BOARD")
+    b.launch()
+    print("OLD MANHATTEN", b.calc_manhatten())
+    s = Solver(b,algo)
+    print("NEW MANHATTEN", s.beststate.calc_manhatten())
+    s.beststate.printArray()
+
+elif algo == 3:
+    b1 = Board(rows,cols,houses)
+    #print("ORIGINAL BOARD")
+    b1.launch()
+    #print("Manhatten Distance:", b1.calc_manhatten())
+
+    for i in range(4):
+        print("*********Start Board*****************")
+        print("Original Manhatten Distance:", b1.calc_manhatten())
+        #b1.printArray()
+
+        s1_1_hillclimb = Solver(deepcopy(b1), 1)
+        print("**********Hillclimb solution w/ Random Restart**********")
+        print("New Random Restart Manhatten Distance:", s1_1_hillclimb.beststate.calc_manhatten())
+        #s1_1_hillclimb.beststate.printArray()
+
+        s1_1_annealing = Solver(deepcopy(b1), 2)
+        print("*****Simulated Annealing solution****")
+        print("New Simulated Annealing Manhatten Distance:", s1_1_annealing.beststate.calc_manhatten())
+        #s1_1_annealing.beststate.printArray()
+        b1.randomize()
+
--- solver.py
+++ solver.py
@@ -29,7 +29,9 @@ class Solver:
                 for state in possible_states:
                     states.append((cpy.copy(state), state.calc_manhatten()))
                 states.sort(key = lambda x:x[1]) # can be used for finding the lowest manhatten distance, index 0 will be new state to move into
-                if states[0][1] < best_state[1]:
+                if len(states) == 0:
+                    pass
+                elif states[0][1] < best_state[1]:
                     best_state = cpy.deepcopy(states[0])
                     current_state = cpy.deepcopy(states[0])
                     newglobal+= 1
@@ -44,98 +46,41 @@ class Solver:
         self.beststate = best_state[0]
         return best_state[0]
 
-    # def simulatedAnnealing(self):
-        
-    #     t = 10000 # tempature
-    #     a = .05
-    #     prob = lambda a : exp(a/t)
-    #     choice = ["true", "false"]
-    #     temp_delta = -2 #best state dist - the worse off state dist
-        
-    #     alpha = prob(temp_delta)
-        
-    #     distro = [alpha, 1-alpha]
-    #     random.seed(datetime.now())
-    #     selection = random.choices(choice, distro)
-
-    #     # decrease tempature
-
-
-
-
-    #     restarts = 0
-    #     best_state = (cpy.deepcopy(self.startState), self.startState.calc_manhatten())
-    #     current_state = (cpy.deepcopy(self.startState), self.startState.calc_manhatten())
-    #     while t > 0:
-    #         possible_states = current_state[0].move()
-    #         states = []
-    #         for state in possible_states:
-    #             states.append((cpy.copy(state), state.calc_manhatten()))
-    #         states.sort(key = lambda x:x[1]) # can be used for finding the lowest manhatten distance, index 0 will be new state to move into
-    #         if states[0][1] < best_state[1]:
-    #             best_state = cpy.deepcopy(states[0])
-    #             current_state = cpy.deepcopy(states[0])
-    #             states = []
-    #             continue
-    #         else:
-    #             t = t * a
-    #             current_state = states[random] #TODO implement random state selection
-    #             temp_delta = current_state[1] - best_state[1]
-    #             states = []
-    #             break
-
-    #     self.beststate = best_state[0]
-    #     return best_state[0]
-
-
-
-
-        """
-        randomly select new state
-        if new state is better than current, current state becomes new state. Save this state as global optimum
-        if new state is worse, has probability of still replacing current state, depending on current temperature, and delta E
-        """
         
     def simulatedAnnealing(self):
-        t = 10000 # tempature
-        a = .05
-        prob_f = lambda x : round(exp(round(x, 3)/round(t,3)), 5)
+        t = 1000000  # tempature
+        a = .99
+        newglobal = 0
+        iterations = -1
+        prob_f = lambda x: round(exp(round(x, 3) / round(t, 3)), 5)
         choice = ["true", "false"]
-        best_state = (cpy.deepcopy(self.startState), self.startState.calc_manhatten())
+        global_best = (cpy.deepcopy(self.startState), self.startState.calc_manhatten())
+        local_best = (cpy.deepcopy(self.startState), self.startState.calc_manhatten())
         current_state = (cpy.deepcopy(self.startState), self.startState.calc_manhatten())
         while t > 1:
+            iterations+=1
             possible_states = current_state[0].move()
             states = []
             for state in possible_states:
                 states.append((cpy.copy(state), state.calc_manhatten()))
-            random_state = states[random.randrange(0,len(states))] # Probably needs fixing
-            delta = best_state[1] - random_state[1]
-            prob = prob_f(delta)
-            distro = [prob, 1-prob]
-            random.seed(datetime.now())
-            selection = random.choices(choice, distro)
-            if delta > 0 or selection == ["true"]:
-                if delta < 0:
-                    t = float(t * a)
-                elif random_state[1] < best_state[1]:
-                    best_state = cpy.deepcopy(random_state)
+            random_state = states[random.randrange(0, len(states))]  # Probably needs fixing
+            delta = current_state[1] - random_state[1]
+            if delta > 0:
                 current_state = cpy.deepcopy(random_state)
-                states = []
-                continue 
-        self.beststate = best_state[0]
-        return best_state[0]    
-
-        
-b = Board(10,10,20)
-b.launch()
-print(b.calc_manhatten())
-s = Solver(b,1)
-s2 = Solver(b,2)
-
-print()
-s.beststate.printArray()
-print(s.beststate.calc_manhatten())
-
-print()
-s2.beststate.printArray()
-print(s2.beststate.calc_manhatten())
+                if current_state[1] < local_best[1]:
+                    local_best = cpy.deepcopy(random_state)
+                if local_best[1] < global_best[1]:
+                    newglobal+=1
+                    global_best = cpy.deepcopy(local_best)
+            else:
+                prob = prob_f(delta)
+                distro = [prob, 1 - prob]
+                random.seed(datetime.now())
+                selection = random.choices(choice, distro)
+                if selection == ["true"]:
+                    current_state = cpy.deepcopy(random_state)
+            t *= a
+            states = []
+        self.beststate = global_best[0]
+        print("Total States Checked:",iterations,",",newglobal,"improved global optimal")
+        return global_best[0]    
